c58ea59eab6004a2e153b5695de8f703ee53886c
jsstringer <jsamuelstringer@gmail.com>
2019-11-29 19:05:52 +1100

    updateJScript

---
+++ script.js
@@ -0,0 +1,35 @@
+//password Genarator
+
+// Set up password display
+// Genarate Password
+    // Set clickListen
+ 
+ 
+    // Ask Questions
+
+    // display the first question
+    // get the first question. q1 = questions[0]
+    render(q1)
+    // set onclick for each choice question
+        //when any choice is clicked
+        // get userChoice from button (which did user choose?)
+        // compare userChoice with answer 
+        // if correct
+        // timer --
+        // render (q2)
+        // alert user
+
+ 
+    // Return password results
+
+
+
+// Copy to clipboard
+        //set clickListener // <button onclick="myFunction()">Copy text</button>
+    //function myFunction() {
+    //    var copyText = document.getElementById("Copy to Clipboard");
+    //    copyText.select();
+    //    copyText.setSelectionRange(0, 99999)
+    //    document.execCommand("copy");
+    //    alert("Copied the text: " + copyText.value);
+    }
