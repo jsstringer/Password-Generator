9f72347a7484cb077c4fa03929225c8e8cb016af
jsstringer <jsamuelstringer@gmail.com>
2019-11-30 09:18:32 +1100

    inprogressJS

--- script.js
+++ script.js
@@ -7,29 +7,49 @@
  
     // Ask Questions
 
-    // display the first question
-    // get the first question. q1 = questions[0]
-    render(q1)
+    for question in my code in need to create a loop so that all the questions are answered
+    
+
+    for (var i = 0; i == questions; i++)
+
+    
+    document.getElementById("Lower-Selector")
+    
+    
     // set onclick for each choice question
+Lower-Selector.addEventListener("click", function(event){
+
+}
+    
+
         //when any choice is clicked
+        document.getElementById("Lower-Selector")
         // get userChoice from button (which did user choose?)
+
         // compare userChoice with answer 
-        // if correct
-        // timer --
-        // render (q2)
+
         // alert user
+
+    set values // 
+    abcdefghijklmnopqrstuvwxyz; ABCDEFGHIJKLMNOPQRSTUVWXYZ; 123456789; !@#$%^&*()_+=
+
+    Password = 
 
  
     // Return password results
+        //create for loop to choose password charaters
+    for(var i = 0; <= ; i++){
+        password =
+    }
 
 
 
 // Copy to clipboard
         //set clickListener // <button onclick="myFunction()">Copy text</button>
     //function myFunction() {
-    //    var copyText = document.getElementById("Copy to Clipboard");
-    //    copyText.select();
-    //    copyText.setSelectionRange(0, 99999)
-    //    document.execCommand("copy");
-    //    alert("Copied the text: " + copyText.value);
+       var copyText = document.getElementById("Copy to Clipboard");
+        copyText.select();
+       copyText.setSelectionRange(0, 99999)
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
     }
