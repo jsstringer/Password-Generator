8bad5679603beee268e87e34c5e01021580472ce
jsstringer <jsamuelstringer@gmail.com>
2019-12-03 22:06:19 +1100

    getEL

--- script.js
+++ script.js
@@ -9,77 +9,89 @@
 
 
 $(document).ready(function () {
+    //use vaules for password selection
+    var LowerSelector = "abcdefghijklmnopqrstuvwyz";
+    var UpperSelector = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var NumSelector = "1234567890";
+    var SpecialSelector = "!@#$%^&*()_+";
 
-
-
-    var lowerCaseLetters = document.getElementById("LowerSelector").value;
-    var upperCaseLetters = document.getElementById("UpperSelector").value;
-
-    
-
+    var LowerSelector = document.getElementById("LowerSelector");
+    var UpperSelector = document.getElementById("UpperSelector");
+    var NumSelector = document.getElementById("NumSelector");
+    var SpecialSelector = document.getElementById("SpecialSelector");
+    var genaratecode = document.getElementById("Genaratecode");
+    var PasswordGenarated = document.getElementById("PasswordGenarated")
+    var NumCharacters = document.getElementById("NumCharacters");
     // checkboxes with new varables
 
+    function password(l,NumCharacters){
+        var password = "";
 
-    function passwordGen() {
-        let values = "abcdefghijklmnopqrstuvwyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+";
-        let password = "";
-
-        
-          
-            
-
-        for (var i = 0; i == values; i++) {
-            password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
-
-
-    document.getElementById("Password-Genarated").value = password;
-
-            //use vaules for password selection
-            //include slectect checked boxes
-            
-
-            //send randpawword to text box
-    //document.getElementById("")
-
-            
-            }
-        
+        // issue how to make the for loop = the new lower, upper, num & special vaules
+        //create for loop to choose password charaters
+        for (var i = 0; i<l; i++){
+            password += NumCharacters.charAt(Math.floor(Math.random() * charaters.length));
         }
+            return password;
+            //use vaules for password selection
+            //include select checked boxes
+            //document.getElementById("")
+
     
-    
-    
+
+    };
+
     // set onclick for each choice question
     //Lower - Selector.addEventListener("click", function (event) {
+    //include select checked boxes
+    $(LowerSelector).on("click", function () {
+        var lower = document.getElementById("LowerSelector");
+        lower.checked = true;
+        console.log("lower");
+    });
 
-        //when any choice is clicked
-        //document.getElementById("Lower-Selector")
-        // get userChoice from button (which did user choose?)
+    $("#UpperSelector").on("click", function () {
+        var upper = document.getElementById("UpperSelector");
+        upper.checked = true;
+        console.log("upper");
+    });
 
-        // compare userChoice with answer 
+    $("#NumSelector").on("click", function () {
+        var num = document.getElementById("NumSelector");
+        num.checked = true;
+        console.log("num");
+    });
 
-        // alert user
+    $("#SpecialSelector").on("click", function () {
+        var special = document.getElementById("SpecialSelector");
+        special.checked = true;
+        console.log("special");
+    
+    $("genaratecode").on("click", function (event) {
+        var characters = "";
+        (LowerSelector.checked) ? characters += LowerSelector : "";
+        (UpperSelector.checked) ? characters += UpperSelector : "";
+        (NumSelector.checked) ? characters += NumCharacters : "";
+        (SpecialSelector.checked) ? characters += SpecialSelector : "";
+        PasswordGenarated.value = password(NumCharacters.value, characters);
+        console.log("passwordGen");
+    });
+    
+    
+    
+});
 
-
+     //when any choice is clicked
+    //document.getElementById("Lower-Selector")
+    // get userChoice from button (which did user choose?)
     // Return password results
-        //create for loop to choose password charaters
+    //send random password to text box
     
 
 
 
-            // Copy to clipboard
-            //set clickListener // <button onclick="myFunction()">Copy text</button>
-            //function myFunction() {
-            //var copyText = document.getElementById("Copy-to-Clipboard");
-            //copyText.select();
-            //copyText.setSelectionRange(0, 99999)
-            //document.execCommand("copy");
-            //alert("Copied the text: " + copyText.value);
-});
 
-$("#LowerSelector").on("click", function () {
-    //if (document.getElementById("#LowerSelector").checked) {
-        // rate_value = document.getElementById().value;
-    console.log("LowerSelector" );
-    //}
-}); // check box true/false
-    // var checkbox
+    // Copy to clipboard
+    //set clickListener // <button onclick="myFunction()">Copy text</button>
+    //function myFunction() {
+})
