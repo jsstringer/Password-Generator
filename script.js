a49badbe65827fb3a8f24951aa0140896ea859bc
jsstringer <jsamuelstringer@gmail.com>
2019-12-03 11:07:16 +1100

    inclasschanges

--- script.js
+++ script.js
@@ -7,15 +7,26 @@
 
 // Ask Questions
 
+
 $(document).ready(function () {
 
     var lowerCaseLetters = document.getElementById("LowerSelector").value;
     var upperCaseLetters = document.getElementById("UpperSelector").value;
 
+    $("#LowerSelector").on("click", function () {
+        //if (document.getElementById("#LowerSelector").checked) {
+            // rate_value = document.getElementById().value;
+        console.log("LowerSelector" );
+        //}
+    });
 
     function passwordGen() {
         let values = "abcdefghijklmnopqrstuvwyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+";
         let password = "";
+
+        
+          
+            
 
         for (var i = 0; i == values; i++) {
             password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
@@ -23,19 +34,15 @@ $(document).ready(function () {
 
     document.getElementById("Password-Genarated").value = password;
 
-    document.getElementById("")
+    //document.getElementById("")
 
-            $("#LowerSelector").on("click", function () {
-                if (document.getElementById("#LowerSelector").checked) {
-                    rate_value = document.getElementById().value;
-                console.log(this)
-                }
+            
             }
         
         }
     
     
-    }
+    
     // set onclick for each choice question
     //Lower - Selector.addEventListener("click", function (event) {
 
@@ -62,4 +69,4 @@ $(document).ready(function () {
             //copyText.setSelectionRange(0, 99999)
             //document.execCommand("copy");
             //alert("Copied the text: " + copyText.value);
-}
+});
