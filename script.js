d1752992855b2678145677a2be24fdf0490b397f
jsstringer <jsamuelstringer@gmail.com>
2020-02-01 16:12:46 +1100

    codefinished

--- script.js
+++ script.js
@@ -1,179 +1,80 @@
-//password Genarator
-
-// Set up password display
-// Genarate Password
-// Set clickListen
-
-
-// Ask Questions
-
 var lowerSelector = "abcdefghijklmnopqrstuvwyz";
 var upperSelector = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numSelector = "1234567890";
 var specialSelector = "!@#$%^&*()_+";
 var passwordGenarated = "";
 var numCharacters = "";
+
 var numCharacters = false;
-var  hasspecialSelector = false;
-
-  // Variable to store boolean regarding the inclusion of numeric characters
-var  hasnumSelector = false;
-
-// Variable to store boolean regarding the inclusion of lowercase characters
-var  haslowerSelector = false;
-
-// Variable to store boolean regarding the inclusion of uppercase characters
+var hasspecialSelector = false;
+var hasnumSelector = false;
+var haslowerSelector = false;
 var hasupperSelector = false;
 
-$(document).ready(function () {
-    //use vaules for password selection
+$(document).ready(function() {
+  characters = [
+    "abcdefghijklmnopqrstuvwyz",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "1234567890",
+    "!@#$%^&*()_+"
+  ];
 
-
-
-    //charsetarray
-    characters = [
-        'abcdefghijklmnopqrstuvwyz',
-        'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-        '1234567890',
-        '!@#$%^&*()_+'];
-    
-    var passwordClick = document.querySelector("#passwordGenarated");
-    var copyPassword = document.querySelector("#copyPW");
-    var resetPassword = document.querySelector("reset");
-
-
-
- numCharacters = parseInt(prompt(
-    "How many characters would you like your password to contain?"
-  ));
-//   parseInt(document.getElementById("NumCharacters").value = numCharacters);
+  numCharacters = parseInt(
+    prompt("How many characters would you like your password to contain?")
+  );
 
   hasspecialSelector = confirm(
     "Click OK to confirm including special characters."
   );
 
-  // Variable to store boolean regarding the inclusion of numeric characters
-  hasnumSelector = confirm(
-    "Click OK to confirm including numeric characters."
+  hasnumSelector = confirm("Click OK to confirm including numeric characters.");
+
+  haslowerSelector = confirm(
+    "Click OK to confirm including lowercase characters."
+  );
+  hasupperSelector = confirm(
+    "Click OK to confirm including uppercase characters."
   );
 
-// Variable to store boolean regarding the inclusion of lowercase characters
-  haslowerSelector = confirm(
-"Click OK to confirm including lowercase characters."
-);
+  pass = [];
 
-// Variable to store boolean regarding the inclusion of uppercase characters
- hasupperSelector = confirm(
-"Click OK to confirm including uppercase characters."
-);
+  function copyClipboard(event) {
+    let password = document.getElementById("thePassword").select();
+    document.execCommand("copy");
+    alert("password add to clipboard");
+  }
 
-pass = [];
-
-
-    
-    
-    function copyClipboard(event) {
-        console.log('hey');
-        let password = document.getElementById("thePassword").select()
-    
-        console.log(password);
-        document.execCommand('copy')
-        
-      //   var copy2Clipboard = document.getElementById("copyPassword");
-      //   copy2Clipboard.select();
-      //   document.execCommand("copy");
-        alert("password add to clipboard");
-        
-        // console.log('hey');
-        // event.preventDefault();
-        // let password = $("#thePassword");
-        // console.log(password.val())
-        // console.log(password)
-        // password.html.select()
-        // document.execCommand("copy");
-        // alert("password add to clipboard");
-    }
-
-
-    $("#copyPassword").on('click', copyClipboard)
-
+  $("#copyPassword").on("click", copyClipboard);
 });
 function passwordClick() {
+  passwordGenarated = "";
 
-    // SUDO
-    // Assume numCharacters  = 6
-    // for (0, 1, 2, 3, 4, 5) {
-    //    generateRandonChar
-    //  
-    // }
-    // 
-    // generateRandonChar
-    //      // what's condition?
-    // var availalbeCharForChoosing = ""
-    // if number, availalbeCharForChoosing += "0123456789"
-    // if lower, availalbeCharForChoosing += "abc.....z"
-    // if upper, availalbeCharForChoosing += "ABC.....Z"
-    // if symbol, availalbeCharForChoosing += "..."
-    // from availalbeCharForChoosing, pick a char at random index
-    // return randomChar
+  for (var ii = 0; ii < numCharacters; ii++) {
+    var availalbeCharForChoosing = "";
 
+    if (haslowerSelector === true) {
+      availalbeCharForChoosing += lowerSelector;
+    }
 
+    if (hasupperSelector === true) {
+      availalbeCharForChoosing += upperSelector;
+    }
 
-        passwordGenarated = "";
-        //event.preventDefult();
-        //var password = {};
-        console.log("line29-passwordfunction");
-        // issue how to make the for loop = the new lower, upper, num & special vaules
-        //create for loop to choose password charaters
-        for (var ii = 0; ii < numCharacters; ii++){
-            
+    if (hasnumSelector === true) {
+      availalbeCharForChoosing += numSelector;
+    }
 
+    if (hasspecialSelector === true) {
+      availalbeCharForChoosing += specialSelector;
+    }
 
-            // generateRandonChar
-            var availalbeCharForChoosing = "";
+    var randonIndex = Math.floor(
+      Math.random() * availalbeCharForChoosing.length
+    );
+    var chosenChar = availalbeCharForChoosing[randonIndex];
 
-            if (haslowerSelector === true) {
-                availalbeCharForChoosing += lowerSelector;
-            }
-            else {
-                console.log("not added");
-            }
+    passwordGenarated += chosenChar;
+  }
 
-            if (hasupperSelector === true){
-                availalbeCharForChoosing += upperSelector;
-            }
-            else {
-                console.log("not added");
-            }
-            if (hasnumSelector === true){
-                availalbeCharForChoosing += numSelector;
-                //password = (result[numSelector] + 1);
-                //password = (NumCharacters + ( 1 * NumSelector));
-            }
-            else {
-                console.log("not added");
-            }
-
-            if (hasspecialSelector === true){
-                availalbeCharForChoosing += specialSelector;
-                
-            }
-            else {
-                console.log("not added");
-            }
-
-            console.log("We are at password char index " + ii + " , char avaiable for randomly picking is " + availalbeCharForChoosing)
-
-            var randonIndex = Math.floor(Math.random() * availalbeCharForChoosing.length);
-            var chosenChar = availalbeCharForChoosing[randonIndex];
-            console.log("random index is " + randonIndex + ', chosenChar is ' + chosenChar);
-
-            passwordGenarated += chosenChar
-
-            // var newChar = passwordGenarated.charAt(index)
-            // pass.push(newChar);
-            //return passwordClick;    
-        };
-        console.log(passwordGenarated);
-        document.getElementById("thePassword").textContent = passwordGenarated;
+  document.getElementById("thePassword").textContent = passwordGenarated;
 }
